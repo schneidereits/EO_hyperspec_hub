b30328c4754fb2d24fc70be71dab6c085780f30d
schneidereits <43445339+schneidereits@users.noreply.github.com>
2021-06-23 09:03:45 +0200

    Update landsat_processing.R

--- landsat_processing.R
+++ landsat_processing.R
@@ -10,12 +10,18 @@ library(randomForest)
 
 # data manipulation ----
 
+# import files
 landsat_files <- list.files("O:/SS21_EO/Hyperspectral/data/landsat/")
 landsat_BOA_files <- landsat_files[grepl("BOA", landsat_files)]
+landsat_QAI_files <- landsat_files[grepl("QAI", landsat_files)]
+
+# QAI raster <- 
+raster <- raster(paste0("O:/SS21_EO/Hyperspectral/data/landsat/",c("20130102"), "_LEVEL2_LND07_QAI",".tif"))
+QAI_count <- freq(raster) %>% as.data.frame() %>% arrange(-count)
+QAI_count
 
 # stack all the temporal metrics
 stack <- stack(paste0("O:/SS21_EO/Hyperspectral/data/landsat/",c("20130102"), "_LEVEL2_LND07_BOA",".tif"))
 plot(stack)
-
 
 
